1fe58ae35f4f94b4762fc7e050826995414c7281
radin-reth <radin.reth@gmail.com>
2016-10-19 17:50:37 +0700

    update sidenav

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ $('.selectpicker').selectpicker({
   style: 'btn-default',
   size: false
 });
+
+// $('#accordion1').on('show.bs.collapse', function () {
+//   debugger;
+// })
+
+$('.collapse').on('show.bs.collapse', function() {
+  $(this).parent().find(".glyphicon-chevron-right").addClass("animate");
+}).on('hide.bs.collapse', function(){
+  $(this).parent().find(".glyphicon-chevron-right").removeClass("animate");
+});
